4b784ec952688e0d423d796093893c09c79249b7
ashmeet28 <112308196+ashmeet28@users.noreply.github.com>
2024-01-02 15:11:06 -0500

    Starting with rust

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use std::env;
+use std::fs;
+
+fn generate_tokens(source_code_bytes: Vec<u8>) {
+    dbg!(source_code_bytes);
+}
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
+    let file_contents = fs::read(file_path).unwrap();
+    generate_tokens(file_contents)
+}
