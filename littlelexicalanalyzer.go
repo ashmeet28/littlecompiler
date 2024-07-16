a5e4ea194f207c2a04d442254b603a6218ea76e2
ashmeet28 <112308196+ashmeet28@users.noreply.github.com>
2024-07-15 20:51:23 -0400

    Progress

--- littlelexicalanalyzer.go
+++ littlelexicalanalyzer.go
@@ -1,62 +1,185 @@
 package main
 
 const (
-	T_ILLEGAL int = iota
-	T_EOF
+	TT_ILLEGAL int = iota
+	TT_EOF
 
-	T_NEW_LINE
+	TT_SPACE
 
-	T_IDENT // main
-	T_INT   // 12345
-	T_CHAR  // 'a'
-	T_STR   // "abc"
+	TT_NEW_LINE
 
-	T_ADD // +
-	T_SUB // -
-	T_MUL // *
-	T_QUO // /
-	T_REM // %
+	TT_IDENT // main
+	TT_INT   // 12345
+	TT_CHAR  // 'a'
+	TT_STR   // "abc"
 
-	T_AND // &
-	T_OR  // |
-	T_XOR // ^
+	TT_ADD // +
+	TT_SUB // -
+	TT_MUL // *
+	TT_QUO // /
+	TT_REM // %
 
-	T_SHL // <<
-	T_SHR // >>
+	TT_AND // &
+	TT_OR  // |
+	TT_XOR // ^
 
-	T_LAND // &&
-	T_LOR  // ||
+	TT_SHL // <<
+	TT_SHR // >>
 
-	T_ARROW // <-
+	TT_LAND // &&
+	TT_LOR  // ||
 
-	T_EQL // ==
-	T_NEQ // !=
-	T_LSS // <
-	T_GTR // >
-	T_LEQ // <=
-	T_GEQ // >=
+	TT_ARROW // <-
 
-	T_ASSIGN // =
+	TT_EQL // ==
+	TT_NEQ // !=
+	TT_LSS // <
+	TT_GTR // >
+	TT_LEQ // <=
+	TT_GEQ // >=
 
-	T_LPAREN // (
-	T_RPAREN // )
+	TT_ASSIGN // =
 
-	T_COMMA // ,
+	TT_LPAREN // (
+	TT_RPAREN // )
 
-	T_FUNC
-	T_RETURN
+	TT_COMMA // ,
 
-	T_IF
-	T_ELSE
+	TT_FUNC
+	TT_RETURN
 
-	T_WHILE
-	T_BREAK
-	T_CONTINUE
+	TT_IF
+	TT_ELSE
 
-	T_LET
+	TT_WHILE
+	TT_BREAK
+	TT_CONTINUE
 
-	T_END
+	TT_LET
+
+	TT_END
 )
+
+type TokenData struct {
+	Kype   int
+	Offset int
+	Buf    []byte
+}
+
+func getTokenType(buf []byte) (int, int) {
+	if len(buf) == 0 {
+		return TT_EOF, 0
+	} else if buf[0] == 0x0a {
+		return TT_NEW_LINE, 1
+	} else if buf[1] == 0x20 {
+		return TT_SPACE, 1
+	}
+
+	var srcLine string
+
+	for i, c := range buf {
+		if c == 0x0a {
+			srcLine = string(buf[:i])
+			break
+		}
+	}
+
+	tokType := TT_ILLEGAL
+	bytesConsumed := 0
+
+	TokTypeToStr := map[int]string{
+		TT_ADD: "+",
+		TT_SUB: "-",
+		TT_MUL: "*",
+		TT_QUO: "/",
+		TT_REM: "%",
+
+		TT_AND: "&",
+		TT_OR:  "|",
+		TT_XOR: "^",
+
+		TT_SHL: "<<",
+		TT_SHR: ">>",
+
+		TT_LAND: "&&",
+		TT_LOR:  "||",
+
+		TT_ARROW: "<-",
+
+		TT_EQL: "==",
+		TT_NEQ: "!=",
+		TT_LSS: "<",
+		TT_GTR: ">",
+		TT_LEQ: "<=",
+		TT_GEQ: ">=",
+
+		TT_ASSIGN: "=",
+
+		TT_LPAREN: "(",
+		TT_RPAREN: ")",
+
+		TT_COMMA: ",",
+
+		TT_FUNC:   "func",
+		TT_RETURN: "return",
+
+		TT_IF:   "if",
+		TT_ELSE: "else",
+
+		TT_WHILE:    "while",
+		TT_BREAK:    "break",
+		TT_CONTINUE: "continue",
+
+		TT_LET: "let",
+
+		TT_END: "end",
+	}
+
+	var prevTokStr string
+	for curTokType, curTokStr := range TokTypeToStr {
+		if (len(srcLine) >= len(curTokStr) && srcLine[:len(curTokStr)] == curTokStr) &&
+			(tokType == TT_ILLEGAL || len(prevTokStr) < len(curTokStr)) {
+
+			tokType = curTokType
+			bytesConsumed = len(curTokStr)
+			prevTokStr = curTokStr
+		}
+	}
+
+	if tokType != TT_ILLEGAL {
+		return tokType, bytesConsumed
+	}
+
+	isDigit := func(c byte) bool {
+		return c >= 0x30 && c <= 0x39
+	}
+
+	isAplabet := func(c byte) bool {
+		return (c >= 0x41 && c <= 0x5a) || (c >= 0x61 && c <= 0x7a) || (c == 0x5f)
+	}
+
+	i := 0
+
+	if isAplabet(srcLine[i]) {
+
+		tokType = TT_IDENT
+		for (i < len(srcLine)) && (isAplabet(srcLine[i]) || isDigit(srcLine[i])) {
+			i++
+		}
+		bytesConsumed = i
+
+	} else if isDigit(srcLine[i]) {
+
+		tokType = TT_INT
+		for (i < len(srcLine)) && (isAplabet(srcLine[i]) || isDigit(srcLine[i])) {
+			i++
+		}
+		bytesConsumed = i
+
+	}
+
+	return tokType, bytesConsumed
+}
 
 func LexicalAnalyzer(buf []byte) {
 
